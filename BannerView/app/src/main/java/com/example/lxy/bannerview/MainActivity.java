1d78ea7817fc3fb7603e0482152da5fb8d315483
paranoia0618 <1947696970@qq.com>
2017-12-21 20:28:34 +0800

    BannerView
    APP常见广告轮播图

---
+++ BannerView/app/src/main/java/com/example/lxy/bannerview/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.lxy.bannerview;
+
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ImageView;
+
+        import com.example.lxy.bannerview.BannerView;
+
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Timer;
+        import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int[] imgs = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d};
+    private List<View> viewList;
+    BannerView bannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewList = new ArrayList<View>();
+        for (int i = 0; i < imgs.length; i++) {
+            ImageView image = new ImageView(this);
+            image.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            //设置显示格式
+            image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            image.setImageResource(imgs[i]);
+            viewList.add(image);
+        }
+        bannerView = (BannerView) findViewById(R.id.banner);
+        bannerView.startLoop(true);
+
+        bannerView.setViewList(viewList);
+//        bannerView.setTransformAnim(true);
+    }
+}
