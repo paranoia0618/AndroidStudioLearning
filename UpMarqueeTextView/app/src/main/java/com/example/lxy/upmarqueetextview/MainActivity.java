5018963a4b41e3333395e5adbdbb71f3e021db8e
paranoia0618 <1947696970@qq.com>
2017-12-24 22:36:52 +0800

    仿淘宝头条

---
+++ UpMarqueeTextView/app/src/main/java/com/example/lxy/upmarqueetextview/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.lxy.upmarqueetextview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 仿淘宝首页的 淘宝头条滚动的自定义View
+ */
+public class MainActivity extends AppCompatActivity {
+
+
+    private UPMarqueeView upview1;
+    List<String> data = new ArrayList<>();
+    List<View> views = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initParam();
+        initdata();
+        initView();
+    }
+
+    /**
+     * 实例化控件
+     */
+    private void initParam() {
+        upview1 = (UPMarqueeView) findViewById(R.id.upview1);
+    }
+
+    /**
+     * 初始化界面程序
+     */
+    private void initView() {
+        setView();
+        upview1.setViews(views);
+        /**
+         * 设置item_view的监听
+         */
+        upview1.setOnItemClickListener(new UPMarqueeView.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, View view) {
+                Toast.makeText(MainActivity.this, "你点击了第几个items" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * 初始化需要循环的View
+     * 为了灵活的使用滚动的View，所以把滚动的内容让用户自定义
+     * 假如滚动的是三条或者一条，或者是其他，只需要把对应的布局，和这个方法稍微改改就可以了，
+     */
+    private void setView() {
+        for (int i = 0; i < data.size(); i = i + 2) {
+            final int position = i;
+            //设置滚动的单个布局
+            LinearLayout moreView = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.item_view, null);
+            //初始化布局的控件
+            TextView tv1 = (TextView) moreView.findViewById(R.id.tv1);
+            TextView tv2 = (TextView) moreView.findViewById(R.id.tv2);
+
+            /**
+             * 设置监听
+             */
+            moreView.findViewById(R.id.rl).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this, position + "你点击了" + data.get(position).toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            /**
+             * 设置监听
+             */
+            moreView.findViewById(R.id.rl2).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this, position + "你点击了" + data.get(position + 1).toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            //进行对控件赋值
+            tv1.setText(data.get(i).toString());
+            if (data.size() > i + 1) {
+                //因为淘宝那儿是两条数据，但是当数据是奇数时就不需要赋值第二个，所以加了一个判断，还应该把第二个布局给隐藏掉
+                tv2.setText(data.get(i + 1).toString());
+            } else {
+                moreView.findViewById(R.id.rl2).setVisibility(View.GONE);
+            }
+
+            //添加到循环滚动数组里面去
+            views.add(moreView);
+        }
+    }
+
+    /**
+     * 初始化数据
+     */
+    private void initdata() {
+        data = new ArrayList<>();
+        data.add("因为快要毕业了，书本低价出售！！！");
+        data.add("iPhone8感人二手价，必须买买买买!!!!");
+        data.add("简直是白菜价！王者玩家低价甩卖游戏号");
+        data.add("口红低价转卖，有意者请尽快联系！！");
+        data.add("二手电脑低价转卖，友情价，速联系！");
+//        data.add("竟不是小米乐视！看水抢了骁龙821首发了！！！");
+
+    }
+}
