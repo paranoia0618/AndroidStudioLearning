a29222cb7e8e13633f45e08189c3be7d494a4e2f
paranoia0618 <1947696970@qq.com>
2018-03-10 23:13:01 +0800

    框架

---
+++ MyApplication/app/src/main/java/com/example/lxy/trade/MyFragment2.java
@@ -0,0 +1,19 @@
+package com.example.lxy.trade;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyFragment2 extends Fragment {
+
+    public MyFragment2() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_lianxiren,container,false);
+        return view;
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/lxy/trade/MyFragment4.java
@@ -0,0 +1,19 @@
+package com.example.lxy.trade;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyFragment4 extends Fragment {
+
+    public MyFragment4() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_wode,container,false);
+        return view;
+    }
+}
